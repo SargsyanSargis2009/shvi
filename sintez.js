8c0b6e70811ef8630cfe4ac89c7592b549d436d0
darthspectrum <muradianator@gmail.com>
2025-05-06 19:38:24 +0400

    :speech_baloon: a better language for instructions

--- sintez.js
+++ sintez.js
@@ -1,4 +1,4 @@
-export { encodeWAV, generatePCM };
+export { encodeWAV, generatePCM, tokenize };
 
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
@@ -9,7 +9,19 @@ export { encodeWAV, generatePCM };
 //   n: Sample number (integer), from 0 to R × duration − 1
 
 function generatePCM(frequency, duration) {
-  throw new Error("Not implemented");
+  const amplitude = 32767;
+  const sampleRate = 44100;
+
+  const numSamples = Math.floor(sampleRate * (duration / 1000));
+
+  const samples = [];
+  for (let i = 0; i < numSamples; i++) {
+    const t = i / sampleRate;
+    const sample = amplitude * Math.sin(2 * Math.PI * frequency * t);
+    samples.push(sample);
+  }
+
+  return samples;
 }
 
 async function encodeWAV(
@@ -51,3 +63,7 @@ async function encodeWAV(
     new Uint8Array(buffer),
   );
 }
+
+const tokenize = (input) => {
+  throw new Error("Not implemented");
+};

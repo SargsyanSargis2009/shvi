f351bcc2eff4ac471e40e4ad4f2ca2c0b0fbf97c
darthspectrum <muradianator@gmail.com>
2025-05-07 20:20:21 +0400

    :mag: more clues on PCM generation

--- sintez.js
+++ sintez.js
@@ -13,6 +13,14 @@ const sampleRate = 44100;
 //   R: Sample rate (samples per second), typically 44100 Hz
 //   n: Sample number (integer), from 0 to R × duration − 1
 
+// sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
+
+// Where:
+//   A: Amplitude (max value based on bit depth, e.g., 32767 for 16-bit)
+//   f: Frequency (Hz), e.g., middle C = 261.63 Hz
+//   R: Sample rate (samples per second), typically 44100 Hz
+//   n: Sample number (integer), from 0 to R × duration − 1
+
 function generatePCM(frequency, duration) {
   function fadeInPart(frequency, fadeSamples) {
     const samples = [];

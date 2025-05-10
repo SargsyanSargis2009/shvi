95660c9065646eceed2d5e3180fa15c8fa61093b
darthspectrum <muradianator@gmail.com>
2025-05-10 15:43:55 +0400

    :construction: interpreting sentences

--- sintez.js
+++ sintez.js
@@ -4,15 +4,6 @@ const amplitude = 32767;
 const sampleRate = 44100;
 
 
-
-// sample[n]= A ⋅ sin(2 * π * f * (n / R))
-
-// Where:
-//   A: Amplitude (max value based on bit depth, e.g., 32767 for 16-bit)
-//   f: Frequency (Hz), e.g., middle C = 261.63 Hz
-//   R: Sample rate (samples per second), typically 44100 Hz
-//   n: Sample number (integer), from 0 to R × duration − 1
-
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
 // Where:
